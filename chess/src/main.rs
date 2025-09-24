1f6a290f7b9db22c65e790619fee8f04d1f0772c
Viggo Carrefors <viggo@5CD2226W84.localdomain>
2025-09-24 17:20:46 +0200

    pawn works and can check for lagal moves

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,12 +1,18 @@
 use std::{array, thread::AccessError};
 
 fn main() {
-    let mut fen:String = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1".to_string();
+    let mut fen:String = "rnbqkbnr/pppppppp/1P4P1/8/8/1p4p1/PPPPPPPP/RNBQKBNR w KQkq e3 0 1".to_string();
+    let mut fen2:String = "rnbqkbnr/pppppppp/1P4P1/8/8/1p4p1/PPPPPPPP/RNBQKBNR b KQkq e3 0 1".to_string();
     println!("{}", fen);
     fen = game_turn(fen, "".to_string());
+    fen2 = game_turn(fen2, "".to_string());
     println!("{}", fen);
     println!("{:?}", board(fen.clone()));
-    let i = checklegalmoves(fen.clone(), actiontranslation("e4 d6".to_string()));
+    let mut i = checklegalmoves(fen.clone(), actiontranslation("h2 h4".to_string()));
+    i = checklegalmoves(fen.clone(), actiontranslation("h2 g3".to_string()));
+    i = checklegalmoves(fen.clone(), actiontranslation("a2 b3".to_string()));
+    i = checklegalmoves(fen2.clone(), actiontranslation("h7 g6".to_string()));
+    i = checklegalmoves(fen2.clone(), actiontranslation("a7 b6".to_string()));
     fen = retranslate(fen);
     println!("{}", fen);
     println!("{:?}", actiontranslation("e4 d6".to_string()));
@@ -99,12 +105,10 @@ fn board(fen:String) -> [[char; 8]; 8] {
         else if i == '/' {
             lat += 1;
             long = 0;
-            println!("d");
         }
         else {
             board[long][lat] = i;
             long += 1;
-            print!("{} ", i);
         }
     }
     return board;
@@ -113,37 +117,84 @@ fn board(fen:String) -> [[char; 8]; 8] {
 fn checklegalmoves(mut fen:String, action: [[usize; 2]; 2]) -> bool {
     let turn: char = fen.split_off(72).split_at(1).0.parse::<char>().unwrap();
     let mut board: [[char; 8]; 8] = board(fen);
-    println!("{:?}", board);
-    println!("{:?}", action);
-    println!("{}", action[0][0]);
-    println!("{}", action[0][1]);
-    println!("{}", board[action[0][0]][action[0][1]]);
-    println!("{}", board[5][5]);
-    if board[action[0][0]][action[0][1]] == 'P'   {
-        println!("dww");
+    if (board[action[0][0]][action[0][1]] == 'P' && turn == 'w') || (board[action[0][0]][action[0][1]] == 'p' && turn == 'b') {
+        println!("dwas");
         return legalpawn(board, action, turn);
     }
-    else if board[action[0][0]][action[0][1]] == 'N' || board[action[0][0]][action[0][1]] == 'n' {
+    else if (board[action[0][0]][action[0][1]] == 'N' && turn == 'w') || (board[action[0][0]][action[0][1]] == 'n' && turn == 'b') {
+
+    }
+    else if (board[action[0][0]][action[0][1]] == 'B' && turn == 'w') || (board[action[0][0]][action[0][1]] == 'b' && turn == 'b') {
+
+    }
+    else if (board[action[0][0]][action[0][1]] == 'R' && turn == 'w') || (board[action[0][0]][action[0][1]] == 'r' && turn == 'b') {
+
+    }
+    else if (board[action[0][0]][action[0][1]] == 'Q' && turn == 'w') || (board[action[0][0]][action[0][1]] == 'q' && turn == 'b') {
+
+    }
+    else if (board[action[0][0]][action[0][1]] == 'K' && turn == 'w') || (board[action[0][0]][action[0][1]] == 'k' && turn == 'b') {
 
     }
     return false;
 }
 
-fn legalpawn(board:[[char; 8]; 8], action: [[usize; 2]; 2], turn: char) -> bool {
-    
-    
-    if turn == 'w' {
+// can't allpasante
+fn legalpawn(board:[[char; 8]; 8], action:[[usize; 2]; 2], turn: char) -> bool {
+    let movedistanse_long = action[0][0] as i32 - action[1][0] as i32;
+    let movedistanse_lat = action[0][1] as i32 - action[1][1] as i32;
+    println!("{}", movedistanse_lat);
+    println!("{}", movedistanse_long);
+    println!("{:?}", action);
+    if movedistanse_long > 1 || movedistanse_long < -1 || movedistanse_lat > 2 || movedistanse_lat < -2 {
+        return false;
+    }
+    else if turn == 'w' && movedistanse_lat > 0{
+        if action[0][1] == 6 && action[1][1] == 4 && movedistanse_long == 0 && board[action[0][0]][action[0][1] - 1] == '-' && board[action[0][0]][action[0][1] - 2] == '-' {
+            return true;
+        }
+        else if movedistanse_long == 0 && movedistanse_lat == 1 && board[action[0][0]][action[0][1] - 1] == '-' {
+            return true;
+        }
+        else if movedistanse_lat == 1 && (movedistanse_long == 1 || movedistanse_long == -1){
+            if movedistanse_long == 1 && action[0][0] != 0 && board[action[0][0] - 1][action[0][1] - 1].is_uppercase() == false && board[action[0][0] - 1][action[0][1] - 1] != '-' {
+                return true;
+            }
+            else if movedistanse_long == -1 && action[0][0] != 7 && board[action[0][0] + 1][action[0][1] - 1].is_uppercase() == false && board[action[0][0] + 1][action[0][1] - 1] != '-' {
+                return true;
+            }
+        }
 
-        if action[0][1] == 6 {
-
+    }
+    else if turn == 'b' && movedistanse_lat < 0{
+        if action[0][1] == 1 && action[1][1] == 3 && movedistanse_long == 0 && board[action[0][0]][action[0][1] + 1] == '-' && board[action[0][0]][action[0][1] + 2] == '-' {
+            return true;
+        }
+        else if movedistanse_long == 0 && movedistanse_lat == -1 && board[action[0][0]][action[0][1] + 1] == '-' {
+            return true;
+        }
+        else if movedistanse_lat == -1 && (movedistanse_long == 1 || movedistanse_long == -1){
+            if movedistanse_long == 1 && action[0][0] != 0 && board[action[0][0] - 1][action[0][1] + 1].is_uppercase() == true && board[action[0][0] - 1][action[0][1] + 1] != '-' {
+                return true;
+            }
+            else if movedistanse_long == -1 && action[0][0] != 7 && board[action[0][0] + 1][action[0][1] + 1].is_uppercase() == true && board[action[0][0] + 1][action[0][1] + 1] != '-' {
+                return true;
+            }
         }
     }
-    
     return false;
 }
 
-fn legalknight(board:[[char; 8]; 8], action: [[usize; 2]; 2]) -> bool {
+fn legalknight(board:[[char; 8]; 8], action:[[usize; 2]; 2]) -> bool {
+    
 
+    return false;
+}
+
+fn legalbishop(board:[[char; 8]; 8], action:[[usize; 2]; 2]) -> bool {
+    if action[0][0] - action[1][0] == action[0][1] - action[1][1] {
+        
+    }
 
     return false;
 }
